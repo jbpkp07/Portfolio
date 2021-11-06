c9c2a5279368091a8ef3a27eab60d2eda9afee5b
jbpkp07 <jeremy.w.barnes@gmail.com>
2021-11-06 01:51:01 -0700

    Style tweaks, adding img rotator

---
+++ assets/js/aboutMeImgRotator.js
@@ -0,0 +1,12 @@
+const aboutMeImgRotator = () => {
+    // const aboutMeImgElement1 = $("#aboutMeImg1");
+    // const aboutMeImgElement2 = $("#aboutMeImg2");
+    
+    // setInterval(async () => {
+    //     aboutMeImgElement1.fadeTo(2000, 0);
+    //     // await aboutMeImgElement1.attr("src", "./assets/images/meDatacenter.jpg").promise();
+    //     aboutMeImgElement2.fadeTo(2000, 1);
+    // }, 5000);
+};
+
+jQuery(aboutMeImgRotator);
