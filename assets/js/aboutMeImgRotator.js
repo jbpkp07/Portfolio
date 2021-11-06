f47ff145bf46421f18628647e41ae179fa9fe6b1
jbpkp07 <jeremy.w.barnes@gmail.com>
2021-11-06 16:27:37 -0700

    Added about me images and removed FriendFinder, added Computer Hangman

--- assets/js/aboutMeImgRotator.js
+++ assets/js/aboutMeImgRotator.js
@@ -1,26 +1,22 @@
 const aboutMeImgRotator = () => {
-    // const aboutMeImgElement1 = $("#aboutMeImg1");
-    // const aboutMeImgElement2 = $("#aboutMeImg2");
-    
-    // setInterval(async () => {
-    //     aboutMeImgElement1.fadeTo(2000, 0);
-    //     // await aboutMeImgElement1.attr("src", "./assets/images/meDatacenter.jpg").promise();
-    //     aboutMeImgElement2.fadeTo(2000, 1);
-    // }, 5000);
 
     var oldImg = $("#aboutMeImg");
-    
+
     var img = new Image();
     img.id = "aboutMeImg";
-    img.src = "./assets/images/meDatacenter.jpg";
+    img.src = "./assets/images/aboutMeDiscGolf.jpg";
 
     var newImg = $(img).hide();
     $("#aboutMeImgContainer").append(img);
-    
-    oldImg.stop(true).fadeOut(2500, function() {
+
+    oldImg.stop(true).fadeOut(2500, function () {
         $(this).remove();
     });
     newImg.fadeIn(2500);
 };
+
+const imgAssetPaths = [
+
+];
 
 jQuery(aboutMeImgRotator);
