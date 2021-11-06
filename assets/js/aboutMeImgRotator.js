f360307e9c170517e3cf73d4b501e880a13c045f
jbpkp07 <jeremy.w.barnes@gmail.com>
2021-11-06 14:21:08 -0700

    Added aboutMeImgRotator placeholder

--- assets/js/aboutMeImgRotator.js
+++ assets/js/aboutMeImgRotator.js
@@ -7,6 +7,20 @@ const aboutMeImgRotator = () => {
     //     // await aboutMeImgElement1.attr("src", "./assets/images/meDatacenter.jpg").promise();
     //     aboutMeImgElement2.fadeTo(2000, 1);
     // }, 5000);
+
+    var oldImg = $("#aboutMeImg");
+    
+    var img = new Image();
+    img.id = "aboutMeImg";
+    img.src = "./assets/images/meDatacenter.jpg";
+
+    var newImg = $(img).hide();
+    $("#aboutMeImgContainer").append(img);
+    
+    oldImg.stop(true).fadeOut(2500, function() {
+        $(this).remove();
+    });
+    newImg.fadeIn(2500);
 };
 
 jQuery(aboutMeImgRotator);
